8adaec6f3f655b274d9d731f7048f5cef5d598f8
hirenkiaku <VladislavAndreev1337@yandex.ru>
2024-09-01 21:38:04 +0500

    some additions to header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,24 +2,50 @@ import styles from './Header.module.css';
 import { NavLink } from 'react-router-dom';
 
 const Header = () => {
+  const linkStyle = ({ isActive }: { isActive: boolean }) => {
+    return {
+      backgroundColor: isActive ? 'rgb(147, 147, 250)' : 'inherit',
+    };
+  };
+
   return (
     <header className={styles['header-container']}>
       <nav>
         <ul className={styles['nav-list']}>
-          <NavLink className={styles['nav-list__item']} to="/hooks">
+          <NavLink
+            style={linkStyle}
+            className={styles['nav-list__item']}
+            to="/hooks"
+          >
             Хуки
           </NavLink>
-          <NavLink className={styles['nav-list__item']} to="/tests">
+          <NavLink
+            style={linkStyle}
+            className={styles['nav-list__item']}
+            to="/tests"
+          >
             Тестирование
           </NavLink>
-          <NavLink className={styles['nav-list__item']} to="/work-with-api">
+          <NavLink
+            style={linkStyle}
+            className={styles['nav-list__item']}
+            to="/work-with-api"
+          >
             Работа с API
           </NavLink>
-          <NavLink className={styles['nav-list__item']} to="/redux">
+          <NavLink
+            style={linkStyle}
+            className={styles['nav-list__item']}
+            to="/redux"
+          >
             Redux
           </NavLink>
         </ul>
       </nav>
+      <div className={styles['header__logo']}>
+        <b>HookCloak</b>
+        <i className="bx bxl-react" style={{ marginTop: '4px' }}></i>
+      </div>
     </header>
   );
 };
